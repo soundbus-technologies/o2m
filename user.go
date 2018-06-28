37d628f6a85733b8860c92a8abc3622d6c4313f7
wangoo <wangoo@soundbus.cn>
2018-06-28 14:18:54 +0800

    allow custom user type

--- user.go
+++ user.go
@@ -7,19 +7,25 @@ package o2m
 import (
 	"gopkg.in/mgo.v2"
 	"github.com/go2s/o2x"
+	"reflect"
 )
 
 type MgoUserStore struct {
 	session    *mgo.Session
 	db         string
 	collection string
+	userType   reflect.Type
 }
 
-func NewUserStore(session *mgo.Session, db string, collection string) (us *MgoUserStore) {
+func NewUserStore(session *mgo.Session, db, collection string, userType reflect.Type) (us *MgoUserStore) {
+	if !o2x.IsUserType(userType) {
+		panic("invalid user type")
+	}
 	us = &MgoUserStore{
 		session:    session,
 		db:         db,
 		collection: collection,
+		userType:   userType,
 	}
 	return
 }
@@ -31,17 +37,16 @@ func (us *MgoUserStore) cHandler(handler func(c *mgo.Collection)) {
 	return
 }
 
-func (us *MgoUserStore) Save(u *o2x.User) (err error) {
+func (us *MgoUserStore) Save(u o2x.User) (err error) {
 	us.cHandler(func(c *mgo.Collection) {
 		err = c.Insert(u)
 	})
 	return
 }
 
-func (us *MgoUserStore) Find(id string) (u *o2x.User, err error) {
+func (us *MgoUserStore) Find(id string) (u o2x.User, err error) {
 	us.cHandler(func(c *mgo.Collection) {
-		user := &o2x.User{}
-
+		user := o2x.NewUser(us.userType)
 		mgoErr := c.FindId(id).One(user)
 		if mgoErr != nil {
 			if mgoErr == mgo.ErrNotFound {
